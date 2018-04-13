b3da91ca9b17dac316b6f58b12142447d0069f5a
王志坤 <wangzhikun@openmailbox.org>
2018-04-13 14:47:31 +0800

    [max_heap] add unit test

---
+++ max_heap/max_heap_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import max_heap
+import unittest
+
+
+class TestMaxHeapMethods(unittest.TestCase):
+
+    def test_max_heapify(self):
+        data = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
+        heap = max_heap.MaxHeap(heap=data)
+        heap.max_heapify(1)
+        expected_data = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+        expected_heap = max_heap.MaxHeap(heap=expected_data)
+        self.assertEqual(repr(heap), repr(expected_heap))
+
+
+if __name__ == '__main__':
+    unittest.main()
